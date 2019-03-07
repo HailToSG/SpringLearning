7431ebb37a8d00274a419d4284391c4f36790563
ezaika <iegorzaika@gmail.com>
2019-03-07 19:50:29 +0300

    init binder for whitespaces validation was added

--- src/main/java/itc/srpringmvc/demo/UserController.java
+++ src/main/java/itc/srpringmvc/demo/UserController.java
@@ -1,8 +1,11 @@
 package itc.srpringmvc.demo;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +14,12 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor trimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, trimmerEditor);
+    }
 
     @RequestMapping("/showForm")
     private String showForm (Model theModel){
